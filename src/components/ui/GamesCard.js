37134340e768166630c0bf51b73329a1ebcdda10
Devran Perdana Malik <devranmalik82@gmail.com>
2026-03-30 16:36:13 +0700

    feat: update GamesCard component with improved styling and responsive design

--- src/components/ui/GamesCard.js
+++ src/components/ui/GamesCard.js
@@ -13,13 +13,14 @@ export default function GamesCard({
 
   const CardInner = (
     <div
-      className={`relative bg-surface-container rounded-[2rem] overflow-hidden transition-all duration-300 group-hover:shadow-2xl group-hover:shadow-secondary-container/20 border border-transparent ${className}`}
+      className={`relative bg-surface-container rounded-4xl overflow-hidden transition-all duration-300 group-hover:shadow-2xl group-hover:shadow-secondary-container/20 border border-transparent ${className}`}
     >
-      <div className="w-full h-52 md:h-[400px] relative hover:scale-105 transition-transform duration-300">
+      <div className="w-full h-52 md:h-100 relative hover:scale-105 transition-transform duration-300">
         {image ? (
           <Image
             src={image}
             alt={title}
+            sizes="(min-width: 768px) 300px, 170px"
             fill
             className="object-cover w-full h-full"
           />
@@ -29,7 +30,7 @@ export default function GamesCard({
           </div>
         )}
 
-        <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent" />
+        <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent" />
 
         <div className="absolute bottom-4 left-4 right-4 text-left ">
           {genre && (
@@ -46,7 +47,7 @@ export default function GamesCard({
   );
 
   return (
-    <div className="w-[170px] md:min-w-[300px] snap-center group">
+    <div className="w-42.5] md:min-w-75 snap-center group">
       {href ? (
         <Link href={href} aria-label={title}>
           {CardInner}
