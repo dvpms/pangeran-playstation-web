4def099f803a19069028a993556564758cf42a6b
Devran Perdana Malik <devranmalik82@gmail.com>
2026-04-18 18:55:51 +0700

    refactor: simplify middleware configuration for admin dashboard routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,5 @@
 // src/middleware.js
 export { default as middleware } from "next-auth/middleware";
 
-// Tentukan rute mana saja yang wajib login
-export const config = {
-  // Kita mengunci semua rute di dalam /admin/dashboard,
-  // tapi membiarkan /admin/login tetap terbuka agar orang bisa masuk.
-  matcher: ["/admin/dashboard/:path*"],
-};
+
+export const config = { matcher: ["/admin/dashboard/:path*"] };
